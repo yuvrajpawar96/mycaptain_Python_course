1225d0294d1b7bc72414fe05a10fb3d761cd391b
yuvrajpawar96 <69378159+yuvrajpawar96@users.noreply.github.com>
2021-01-21 13:24:22 +0530

    Program Calculating area of circle by Yuvraj Pawar
    output :
    C:\Users\-\PycharmProjects\mycaptaincourse\venv\Scripts\python.exe C:/Users/-/PycharmProjects/mycaptaincourse/area_of_CIRCLE.py
    Input the radius of the circle : 5
    The area of th circle with radius 5.0 is 78.5
    
    Process finished with exit code 0

---
+++ area_of_CIRCLE.py
@@ -0,0 +1,6 @@
+def area():
+    rad = float(input("Input the radius of the circle : "))
+    pi = 3.14
+    ar_circle = pi*rad*rad
+    print("The area of th circle with radius",rad,"is",ar_circle)
+area()
